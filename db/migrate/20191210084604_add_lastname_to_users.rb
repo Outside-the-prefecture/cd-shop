fd778262d052eb0c5703c0ac7a6f7a8671271a1f
Kenshirou09 <nikikenken@gmail.com>
2019-12-10 09:52:37 +0000

    登録画面の変更

---
+++ db/migrate/20191210084604_add_lastname_to_users.rb
@@ -0,0 +1,12 @@
+class AddLastnameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :last_name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :kana_last_name, :string
+    add_column :users, :kana_first_name, :string
+    add_column :users, :postal_code, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :address, :string
+    add_column :users, :is_deleted, :boolean
+  end
+end
