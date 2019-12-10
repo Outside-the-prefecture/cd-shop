d733f673a4f5eadc91b5c8324fbc3e69a9afed5d
aguro708 <zhiyeahei878@gmail.com>
2019-12-10 11:43:04 +0000

    controller

--- app/controllers/admin/labels_controller.rb
+++ app/controllers/admin/labels_controller.rb
@@ -1,4 +1,22 @@
 class Admin::LabelsController < ApplicationController
 	def create
+		label=Label.new(label_params)
+		if label.save
+			redirect_to admin_manager_products_path
+		else
+			@label=Label.new
+			@artist=Artist.new
+			@genre=Genre.new
+			@product=Product.new
+			@labels=Label.all
+			@artists=Artists.all
+			@genres=Genre.all
+			render "manager_product/new"
+		end
+	end
+
+	private
+	def label_params
+		params.require(:label).permit(:name)
 	end
 end
