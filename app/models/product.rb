6909a22bf7184fe647fe9e9a6b5c6307253cd837
Kenshirou09 <nikikenken@gmail.com>
2019-12-11 05:23:58 +0000

    アソシエーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+	has_many :arrivals
+	has_many :cart_items
+	has_many :order_items
+
+	has_many :discs, dependent: :destroy
+	accepts_nested_attributes_for :discs, allow: true
+
+	belongs_to :artist
+	belongs_to :label
+	belongs_to :genre
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+ 		has_many :orders
+ 		has_many :addresses
+ 		has_many :cart_items
+
+ end
