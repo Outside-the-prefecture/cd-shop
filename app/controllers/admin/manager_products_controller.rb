753f1b360352134e217ee0ddb06bbf9ba34dfa73
aguro708 <zhiyeahei878@gmail.com>
2019-12-11 05:57:19 +0000

    controller

--- app/controllers/admin/manager_products_controller.rb
+++ app/controllers/admin/manager_products_controller.rb
@@ -1,8 +1,14 @@
 class Admin::ManagerProductsController < ApplicationController
 	def index
+		@products=Product.all
 	end
 
 	def show
+		@product=Product.find(params[:id])
+		@discs=@product.discs
+		@orderitems=@product.order_items
+		@arrivals=@product.arrivals
+		@arrival=Arrival.new
 	end
 
 	def new
@@ -10,8 +16,6 @@ class Admin::ManagerProductsController < ApplicationController
 			@artist=Artist.new
 			@genre=Genre.new
 			@product=Product.new
-			@disc=@product.discs.build
-			@song=@disc.songs.build
 			@labels=Label.all
 			@artists=Artist.all
 			@genres=Genre.all
@@ -26,24 +30,41 @@ class Admin::ManagerProductsController < ApplicationController
 			@artist=Artist.new
 			@genre=Genre.new
 			@product=Product.new
+			@disc=@product.discs.build
+			@song=@disc.songs.build
 			@labels=Label.all
 			@artists=Artist.all
 			@genres=Genre.all
-			render "new"
+			render :new
+		end
 	end
 
 	def edit
+		@product=Product.find(params[:id])
 	end
 
 	def update
+		@product=Product.find(params[:id])
+		if @product.update(update_product_params)
+			redirect_to admin_manager_products_path
+		else
+			render :edit
+		end
 	end
 
 	def destroy
+		@product=Product.find(params[:id])
+		@product.destroy
+		redirect_to admin_manager_products_path
 	end
 
 	private
 	def product_params
-		params.require(:product).permit(:name,:jacket_image,:price,:release_date,:artist_id,:label_id,:genre_id, discs_attributes: [:name,:cd_id, songs_attributes: [:name,:disc_id,:song_order]])
+		params.require(:product).permit(:name,:jacket_image,:price,:release_date,:artist_id,:label_id,:genre_id, discs_attributes: [:name,:product_id, songs_attributes: [:name,:disc_id,:song_order]])
+	end
+
+	def product_params
+		params.require(:product).permit(:name,:jacket_image,:price,:release_date,:artist_id,:label_id,:genre_id, discs_attributes: [:name,:product_id,:id,:_destroy, songs_attributes: [:name,:disc_id,:song_order,:id,:_destroy]])
 	end
 
 end
