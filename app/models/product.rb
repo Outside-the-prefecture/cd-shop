95f324392e11dac6f2915857a0376757558b9af9
aguro708 <zhiyeahei878@gmail.com>
2019-12-11 08:59:18 +0000

    model

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,5 +10,11 @@ class Product < ApplicationRecord
 	belongs_to :artist
 	belongs_to :label
 	belongs_to :genre
-
+        def Genre.search(search, artist_or_genre_or_song_or_product)
+          if artist_or_genre_or_song_or_product=="4"
+              Product.where(['name LIKE ?', "#{search}"])
+        	else
+        	  Product.all
+        	end
+        end
 end
