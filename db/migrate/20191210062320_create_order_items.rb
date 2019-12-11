b1af657eefc48d2be2fb4f86c80b8ab5faa0daa2
aguro708 <zhiyeahei878@gmail.com>
2019-12-11 07:23:23 +0000

    modify

--- db/migrate/20191210062320_create_order_items.rb
+++ db/migrate/20191210062320_create_order_items.rb
@@ -3,7 +3,7 @@ class CreateOrderItems < ActiveRecord::Migration[5.2]
     create_table :order_items do |t|
     	t.integer :end_user_id
     	t.integer :product_id
-    	t.integer :nember
+    	t.integer :number
     	t.integer :tax
     	t.integer :price
 
