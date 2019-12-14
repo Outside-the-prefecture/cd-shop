6ce98f942ec161f44b66e889886633cbf2c41735
aguro708 <zhiyeahei878@gmail.com>
2019-12-14 08:03:11 +0000

    controller

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,4 +1,7 @@
 class HistoriesController < ApplicationController
 	def show
+		@order=Order.find(params[:id])
+		@orderitems=@order.order_items
+		@user=User.find(id: [:user_id])
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,13 @@ class UsersController < ApplicationController
 	end
 
 	def update
+
 	end
 
 	def destroy
+		@user=User.find(params[:id])
+		@user.destroy
+		redirect_to root_path
 	end
 
 	def search
