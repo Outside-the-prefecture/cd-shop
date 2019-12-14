a485d4fa99a77114985e15d705145ee894e7113c
Kenshirou09 <nikikenken@gmail.com>
2019-12-14 05:35:47 +0000

    画像サイズ変更

--- db/schema.rb
+++ db/schema.rb
@@ -129,7 +129,6 @@ ActiveRecord::Schema.define(version: 2019_12_11_082528) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "last_name"
