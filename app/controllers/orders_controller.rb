03ceb7baa61d092101086984488a89495d8abba5
ARISA0802 <ringorira3341@gmail.com>
2019-12-15 10:33:31 +0000

    支払い方法途中経過

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
 	def new
+		@user = User.find(params[:user_id])
+		@address = @user.addresses
 	end
 	def create
+		@user = current_user
+		if @user.save
+			redirect _to user_registration_path(@user.id)
+		end
 	end
 	def finish
 	end
