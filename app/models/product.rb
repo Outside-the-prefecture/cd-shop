ee529de1f4e155e9a41ab8924855092c7a541878
Kenshirou09 <nikikenken@gmail.com>
2019-12-18 11:06:07 +0000

    css3

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,6 @@ class Product < ApplicationRecord
 	validates :release_date, presence: true
 	validates :discs, presence: true
 
-
 	has_many :arrivals
 	has_many :cart_items
 	has_many :users, through: :cart_items
@@ -17,12 +16,9 @@ class Product < ApplicationRecord
 	has_many :discs, dependent: :destroy
 	accepts_nested_attributes_for :discs, allow_destroy: true
 
-
-	belongs_to :artist, optional: true
-	belongs_to :label, optional: true
-	belongs_to :genre, optional: true
-
-
+	belongs_to :artist
+	belongs_to :label
+	belongs_to :genre
 
 	attachment :jacket_image
 
