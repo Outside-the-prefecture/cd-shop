42c6c7fa1965ab72ff2e6268310b20b94b2c1be4
aguro708 <zhiyeahei878@gmail.com>
2019-12-20 14:09:31 +0000

    cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -8,7 +8,15 @@ class CartItemsController < ApplicationController
 	end
 
 	def create
+		@usercarts=current_user.cart_items
 	    @product=Product.find(params[:cart_item][:product])
+        if @cartitemequal=@usercarts.find_by(product_id: @product.id)
+		@count = params[:cart_item][:count]
+        @cartitemequal.count=@cartitemequal.count.to_i + @count.to_i
+        @cartitemequal.save
+        flash[:notice]="カートに#{@count}個入りました"
+        redirect_to product_path(@product.id)
+    else
 		@cartitem=CartItem.new
 		@cartitem.count = params[:cart_item][:count]
 		@cartitem.product_id =@product.id
@@ -26,6 +34,7 @@ class CartItemsController < ApplicationController
 			@arrivals=@product.arrivals
 			render "products/show"
 	    end
+	end
 	end
 
 	def update
