300613fe2b8059dce0f57a73b0f38225452208b0
aguro708 <zhiyeahei878@gmail.com>
2019-12-21 06:12:06 +0000

    adminlogin

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,6 +2,6 @@ class HistoriesController < ApplicationController
 	def show
 		@order=Order.find(params[:id])
 		@orderitems=@order.order_items
-		@user=User.find(id: [:user_id])
+		@user=User.find(params[:user_id])
 	end
 end
